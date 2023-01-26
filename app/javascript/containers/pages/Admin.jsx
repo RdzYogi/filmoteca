3986619566b84a4975a656a143a4d345d408aeec
Silviu Rosu <silviurosu1988@gmail.com>
2023-01-26 13:25:32 +0100

    regex grouping by days

--- app/javascript/containers/pages/Admin.jsx
+++ app/javascript/containers/pages/Admin.jsx
@@ -10,59 +10,63 @@ import { Buffer } from "buffer";
 function Admin() {
   const [selectedFile, setSelectedFile] = useState()
 	const [isFilePicked, setIsFilePicked] = useState(false);
+  const [isFileProcessed, setIsFileProcessed] = useState(false);
   const [outputString, setOutputString] = useState("")
+
+  const [days, setDays] = useState([])
   window.Buffer = window.Buffer || Buffer;
 
-  // Triggered when the file is selected
-  //
+  // Triggered when the file is chosen
   useEffect(() => {
     if (isFilePicked) {
       // Read the file
       docx4js.load(selectedFile).then(function (docx) {
 
-        // Select only the text information
-        // console.log(docx.officeDocument.content("w\\:body")["0"].children)
+        // Return the body of the document
+        // console.log(docx.officeDocument.content("w\\:body"))
         let output = ""
 
-        // Map over the text information and return the text with paragraph information
+        // Map over the body information and return the text with paragraph information
+        // To take into account new lines
         const body = docx.officeDocument.content("w\\:body")["0"].children
         Object.keys(body).forEach(key => {
           body[key].children.forEach(child => {
+            // Check if the child is a paragraph
             if (child.name === "w:r"){
+              // Map over the paragraph and return the text
               Object.keys(child.children).forEach(childKey => {
+                // Check if the child is a text node
                 if (child.children[childKey].name === "w:t") {
                   output = output + child.children[childKey].children[0].data
                 }
               })
             }
           })
-          output = output + "\n"
+          // Add a new line after each paragraph
+          output = output + " \n "
         })
-        console.log(output);
-
-        // // Map over the text information and return the text without paragraph information
-        // const text = docx.officeDocument.content("w\\:t")
-        // Object.keys(text).forEach(key => {
-        //   if (text[key].type === "tag") {
-        //     // console.log(text[key].attribs)
-
-        //     // If the text is a new line, add a new line
-        //     if (text[key].attribs["xml:space"] === "preserve") {
-        //       output = output + " "
-        //     }
-
-        //     text[key].children.forEach(child => {
-        //       output = output + child.data
-        //     })
-        //     // console.log("output", test);
-        //   }
-        // } )
-        // // console.log(output);
-        // setOutputString(output)
+        // console.log(output);
+        // Save the output string to the state
+        setOutputString(output)
+        setIsFileProcessed(true)
       })
-      // debugger
     }
   }, [selectedFile])
+
+  // Triggered after the file is processed
+  useEffect(() => {
+    // Check is necessary to avoid triggering the effect on the first render
+    if (isFileProcessed) {
+      // Regex to split into days
+      // console.log(outputString);
+      const daysRegex = /(\S*\s\d{1,2}\sde\s\w*)/
+      const days = outputString.split(daysRegex)
+      setDays(days)
+
+      console.log(days)
+
+    }
+  }, [outputString])
 
   const changeHandler = (event) => {
 		setSelectedFile(event.target.files[0]);
@@ -75,10 +79,10 @@ function Admin() {
     <Layout>
       <Navbar/>
       <div className="pt-20">
-      <input type="file" name="file" onChange={changeHandler} />
-			<div>
-				<button onClick={handleSubmission}>Submit</button>
-			</div>
+        <input type="file" name="file" onChange={changeHandler} />
+        <div>
+          <button onClick={handleSubmission}>Submit</button>
+        </div>
       </div>
       <Footer/>
     </Layout>
