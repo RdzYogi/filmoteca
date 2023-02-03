6f541b04c3e8b1b8dd1cb11d8050c587cf172d98
Vito Lattanzio <vlattanzio13@gmail.com>
2023-02-03 13:32:31 +0100

    Title and Descargar button

--- app/javascript/containers/pages/Cartelera.jsx
+++ app/javascript/containers/pages/Cartelera.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Footer from '../../components/navigation/Footer'
 import Navbar from '../../components/navigation/Navbar'
+import DownloadButton from '../../components/shared/DownloadButton'
 import Layout from '../../hocs/layouts/Layout'
 
 function Cartelera() {
   return (
     <Layout>
       <Navbar/>
-      <div className="pt-40">Cartelera</div>
+      <div className="pt-40 mx-40">
+        <div className="flex justify-center">
+          <div className="">
+            <h1 className="text-2xl font-bold text-center">Cartelera</h1>
+            <div className="absolute top-40 right-40">
+              <DownloadButton/>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer/>
     </Layout>
   )
