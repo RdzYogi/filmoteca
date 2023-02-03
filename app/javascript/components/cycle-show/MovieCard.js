f95e5543876fbd845ab1e565ecb3e9e5239b33e1
Laura Nguyen Trong <laurangt@yahoo.com>
2023-02-03 14:18:25 +0100

    cyclo show page + movieCard (commented out navbar cause logo doesnt show up so couldn't see whole page)

--- app/javascript/components/cycle-show/MovieCard.js
+++ app/javascript/components/cycle-show/MovieCard.js
@@ -3,14 +3,26 @@ import { Navigate } from "react-router-dom"
 
 function MovieCard(props){
   return (
-    <div>
-      <p>{props.title}</p>
-      <p>{props.director} {props.year}</p>
-      {props.session.datetime.forEach(date =>{
-        <p>{date} - {props.hall} - <button>comprar</button></p>
-      })}
-      <img src={props.img_url} alt={props.title}/>
-      <p>{props.description}</p>
+    <div className="border border-black">
+      <p className="text-center font-bold text-xl">{props.title}</p>
+      <p className="text-center">{props.director} ({props.year})</p>
+      {/* {props.session.datetime.forEach(date =>{ */}
+        {/*
+        <div className="flex text-xs bg-black text-white pl-2 pr-2 pt-1 pb-1 justify-between items-center">
+        <p>{date}</p>
+        <p>{props.hall}</p>
+        <p className="font-bold bg-green-700 p-1 "><button>Comprar</button></p>
+        */}
+      {/* })} */}
+      <div className="flex text-xs bg-black text-white pl-2 pr-2 pt-1 pb-1 justify-between items-center">
+        <p>17 enero</p> {/*delete later*/}
+        <p>18:00h</p> {/*delete later*/}
+        <p>{props.hall}</p> {/*delete later*/}
+        <p className="font-bold bg-green-700 p-1 ">Comprar</p> {/*delete later*/}
+
+      </div>
+      {/* <img src={props.img_url} alt={props.title}/> */}
+      <p className="text-justify">{props.description}</p>
     </div>
   )
 }
--- app/javascript/containers/pages/Cycle.jsx
+++ app/javascript/containers/pages/Cycle.jsx
@@ -7,15 +7,40 @@ import MovieCard from '../../components/cycle-show/MovieCard'
 function Cycle(props) {
   return (
     <Layout>
-      <Navbar/>
-      <div className="pt-40">
-        <h1>This is Cycle show</h1>
-        <h2>{props.name}</h2>
-        <p>{props.description}</p>
+      {/* <Navbar/> */}
+      <div className="pt-40 max-w-7xl mx-auto mt-6 pb-1">
+        <h2 className="text-center font-bold text-2xl	pt-2 pb-2 bg-green-700">Douglas Sirk. LA EMOCIÓN INMEDIATA</h2>{/* <h2>{props.name}</h2> */}
+        <p className="text-justify mt-3 mb-7">¿Se puede decir algo nuevo sobre esta gran estrella del mundo del espectáculo español? ¿Queda algo que podamos apuntar acerca de una persona que rompió todos los moldes en un tiempo en el que ser notoria aludía a un horizonte no apto para las mujeres? ¿Puede alguien ser merecedora de la Gran Cruz de la Orden de Isabel Católica, otorgada en pleno franquismo, y de la muy democrática Medalla de Oro al Mérito en el trabajo? ¿Cómo puede una persona reunir en sí tantas posibilidades? Pionera de la resistencia frente a la violencia de género y adalid de una minoría étnica; cabeza incontestable de familia, pero amante fogosa del amor y la pasión sexual; voz alzada frente al mercado de la droga, pero ciudadana procesada por no cumplir con sus obligaciones fiscales; estrella popular, orgullosa de serlo, pero víctima también de esa fama</p>{/* <p>{props.description}</p> */}
+        {/* {props.description.length > x-characters && <button>ver más</button>} */}
+
+        <div className="grid grid-cols-3 gap-4">
+          {/* {props.movies.filter(movie.cycle === props.id).forEach(movie => { */}
+            {/* <MovieCard {...movie}/> */}
+          {/* })} */}
+          <MovieCard
+            title="Magnificent Obsession"
+            director="Douglas Sirk"
+            year="1954"
+            hall="Sala 1"
+            description="Un panadero que se ha hecho rico monta una fábrica de pasta. Un amigo, harto de su superficialidad, decide gastarle una broma y le cuenta que un príncipe va a ir a visitarla. Debut en el largometraje de Detlef Sierck/ Douglas Sirk, siguiendo el sistema de dobles versiones tan común en la época fue rodada también en holandés, aunque esa versión se ha perdido."
+          />
+          <MovieCard
+            title="Magnificent Obsession"
+            director="Douglas Sirk"
+            year="1954"
+            hall="Sala 1"
+            description="Un panadero que se ha hecho rico monta una fábrica de pasta. Un amigo, harto de su superficialidad, decide gastarle una broma y le cuenta que un príncipe va a ir a visitarla. Debut en el largometraje de Detlef Sierck/ Douglas Sirk, siguiendo el sistema de dobles versiones tan común en la época fue rodada también en holandés, aunque esa versión se ha perdido."
+          />
+          <MovieCard
+            title="Magnificent Obsession"
+            director="Douglas Sirk"
+            year="1954"
+            hall="Sala 1"
+            description="Un panadero que se ha hecho rico monta una fábrica de pasta. Un amigo, harto de su superficialidad, decide gastarle una broma y le cuenta que un príncipe va a ir a visitarla. Debut en el largometraje de Detlef Sierck/ Douglas Sirk, siguiendo el sistema de dobles versiones tan común en la época fue rodada también en holandés, aunque esa versión se ha perdido."
+          />
+
+        </div>
       </div>
-        {props.movies.filter(movie.cycle === props.id).forEach(movie => {
-          <MovieCard {...movie}/>
-        })}
       <Footer/>
     </Layout>
   )
