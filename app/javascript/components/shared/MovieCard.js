42f800af74b388dea396fa301198a32e9b097dce
Laura Nguyen Trong <laurangt@yahoo.com>
2023-02-06 10:47:20 +0100

    moviecard into shared +  moviecard(movie) not props

--- app/javascript/components/shared/MovieCard.js
+++ app/javascript/components/shared/MovieCard.js
@@ -1,28 +1,28 @@
 import React, { useState } from "react"
 import { Navigate } from "react-router-dom"
 
-function MovieCard(props){
+function MovieCard(movie){
   return (
     <div className="border border-black">
-      <p className="text-center font-bold text-xl">{props.title}</p>
-      <p className="text-center">{props.director} ({props.year})</p>
-      {/* {props.session.datetime.forEach(date =>{ */}
+      <p className="text-center font-bold text-xl">{movie.title}</p>
+      <p className="text-center">{movie.director} ({movie.year})</p>
+      {/* {movie.session.datetime.forEach(date =>{ */}
         {/*
         <div className="flex text-xs bg-black text-white pl-2 pr-2 pt-1 pb-1 justify-between items-center">
         <p>{date}</p>
-        <p>{props.hall}</p>
+        <p>{movie.hall}</p>
         <p className="font-bold bg-green-700 p-1 "><button>Comprar</button></p>
         */}
       {/* })} */}
-      {/* <img src="props.img_url" alt={props.title}> */}
+      {/* <img src="movie.img_url" alt={movie.title}> */}
       <div className="flex text-xs bg-black text-white pl-2 pr-2 pt-1 pb-1 justify-between items-center">
         <p>17 enero</p> {/*delete later*/}
         <p>18:00h</p> {/*delete later*/}
-        <p>{props.hall}</p> {/*delete later*/}
+        <p>{movie.hall}</p> {/*delete later*/}
         <p className="font-bold bg-green-700 p-1 ">Comprar</p> {/*delete later*/}
       </div>
       <img className="object-cover w-screen" src="https://media.istockphoto.com/id/835370890/photo/sunset-sunrise-with-clouds-light-rays-and-other-atmospheric-effect.jpg?s=612x612&w=0&k=20&c=zGDOBYVFY74wX2gUgkonYGtNl1zenev5mPotAqUlJbM="/>  {/*delete later*/}
-      <p className="text-justify p-2">{props.description}</p>
+      <p className="text-justify p-2">{movie.description}</p>
     </div>
   )
 }
--- app/javascript/containers/pages/Cycle.jsx
+++ app/javascript/containers/pages/Cycle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from '../../components/navigation/Footer'
 import Navbar from '../../components/navigation/Navbar'
 import Layout from '../../hocs/layouts/Layout'
-import MovieCard from '../../components/cycle-show/MovieCard'
+import MovieCard from '../../components/shared/MovieCard'
 
 function Cycle(props) {
   return (
@@ -12,10 +12,9 @@ function Cycle(props) {
         <h2 className="text-center font-bold text-2xl	pt-2 pb-2 bg-green-700">Douglas Sirk. LA EMOCIÓN INMEDIATA</h2>{/* <h2>{props.name}</h2> */}
         <p className="text-justify mt-3 mb-7">¿Se puede decir algo nuevo sobre esta gran estrella del mundo del espectáculo español? ¿Queda algo que podamos apuntar acerca de una persona que rompió todos los moldes en un tiempo en el que ser notoria aludía a un horizonte no apto para las mujeres? ¿Puede alguien ser merecedora de la Gran Cruz de la Orden de Isabel Católica, otorgada en pleno franquismo, y de la muy democrática Medalla de Oro al Mérito en el trabajo? ¿Cómo puede una persona reunir en sí tantas posibilidades? Pionera de la resistencia frente a la violencia de género y adalid de una minoría étnica; cabeza incontestable de familia, pero amante fogosa del amor y la pasión sexual; voz alzada frente al mercado de la droga, pero ciudadana procesada por no cumplir con sus obligaciones fiscales; estrella popular, orgullosa de serlo, pero víctima también de esa fama</p>{/* <p>{props.description}</p> */}
         {/* {props.description.length > x-characters && <button>ver más</button>} */}
-
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {/* {props.movies.filter(movie.cycle === props.id).forEach(movie => { */}
-            {/* <MovieCard {...movie}/> */}
+            {/* <MovieCard movie={movie}/> */}
           {/* })} */}
           <MovieCard
             title="Magnificent Obsession"
@@ -38,7 +37,6 @@ function Cycle(props) {
             hall="Sala 1"
             description="Un panadero que se ha hecho rico monta una fábrica de pasta. Un amigo, harto de su superficialidad, decide gastarle una broma y le cuenta que un príncipe va a ir a visitarla. Debut en el largometraje de Detlef Sierck/ Douglas Sirk, siguiendo el sistema de dobles versiones tan común en la época fue rodada también en holandés, aunque esa versión se ha perdido."
           />
-
         </div>
       </div>
       <Footer/>
