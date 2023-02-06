3be9247b0a2f0dff63340f476461d3284ef5ec52
Silviu Rosu <silviurosu1988@gmail.com>
2023-02-06 14:45:45 +0100

    fixed useState

--- app/javascript/components/navigation/shared/CycleCard.js
+++ app/javascript/components/navigation/shared/CycleCard.js
@@ -8,11 +8,11 @@ function CycleCard(cycle) {
         bottom-0 left-0'>
         <p className='text-xs flex height-5/6 -rotate-90'>CICLO CICLO CICLO CICLO CICLO CICLO CICLO</p>
       </div>
-      <h1 className='text-2xl font-bold'>{cycle.name.toUpperCase()}</h1>
+      <h1 className='text-2xl font-bold'>{cycle.cycle.name}</h1>
       <p className='text-xl'>{cycle.start_date} - {cycle.end_date}</p>
       <img className="h-32 w-48 mb-3 place-self-center"src={cycle.img_url} alt={cycle.name}/>
       {/* repeat */}
-      <h1 className='text-2xl font-bold'>{cycle.name.toUpperCase()}</h1>
+      <h1 className='text-2xl font-bold'>{cycle.name}</h1>
       <p className='text-xl'>{cycle.start_date} - {cycle.end_date}</p>
       <img className="h-32 w-48 place-self-center overflow-x-hidden"src={cycle.img_url} alt={cycle.name}/>
     </div>
