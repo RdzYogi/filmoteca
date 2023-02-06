9fd5bd561acea2e7de2aa7b1f617ddfa5eabdfe4
Laura Nguyen Trong <laurangt@yahoo.com>
2023-02-06 10:15:41 +0100

    responsive cicloshow and moviecard padding+img

--- app/javascript/components/cycle-show/MovieCard.js
+++ app/javascript/components/cycle-show/MovieCard.js
@@ -14,15 +14,15 @@ function MovieCard(props){
         <p className="font-bold bg-green-700 p-1 "><button>Comprar</button></p>
         */}
       {/* })} */}
+      {/* <img src="props.img_url" alt={props.title}> */}
       <div className="flex text-xs bg-black text-white pl-2 pr-2 pt-1 pb-1 justify-between items-center">
         <p>17 enero</p> {/*delete later*/}
         <p>18:00h</p> {/*delete later*/}
         <p>{props.hall}</p> {/*delete later*/}
         <p className="font-bold bg-green-700 p-1 ">Comprar</p> {/*delete later*/}
-
       </div>
-      {/* <img src={props.img_url} alt={props.title}/> */}
-      <p className="text-justify">{props.description}</p>
+      <img className="object-cover w-screen" src="https://media.istockphoto.com/id/835370890/photo/sunset-sunrise-with-clouds-light-rays-and-other-atmospheric-effect.jpg?s=612x612&w=0&k=20&c=zGDOBYVFY74wX2gUgkonYGtNl1zenev5mPotAqUlJbM="/>  {/*delete later*/}
+      <p className="text-justify p-2">{props.description}</p>
     </div>
   )
 }
--- app/javascript/containers/pages/Cycle.jsx
+++ app/javascript/containers/pages/Cycle.jsx
@@ -8,12 +8,12 @@ function Cycle(props) {
   return (
     <Layout>
       {/* <Navbar/> */}
-      <div className="pt-40 max-w-7xl mx-auto mt-6 pb-1">
+      <div className="pt-40 max-w-7xl mx-auto pb-1 my-6">
         <h2 className="text-center font-bold text-2xl	pt-2 pb-2 bg-green-700">Douglas Sirk. LA EMOCIÓN INMEDIATA</h2>{/* <h2>{props.name}</h2> */}
         <p className="text-justify mt-3 mb-7">¿Se puede decir algo nuevo sobre esta gran estrella del mundo del espectáculo español? ¿Queda algo que podamos apuntar acerca de una persona que rompió todos los moldes en un tiempo en el que ser notoria aludía a un horizonte no apto para las mujeres? ¿Puede alguien ser merecedora de la Gran Cruz de la Orden de Isabel Católica, otorgada en pleno franquismo, y de la muy democrática Medalla de Oro al Mérito en el trabajo? ¿Cómo puede una persona reunir en sí tantas posibilidades? Pionera de la resistencia frente a la violencia de género y adalid de una minoría étnica; cabeza incontestable de familia, pero amante fogosa del amor y la pasión sexual; voz alzada frente al mercado de la droga, pero ciudadana procesada por no cumplir con sus obligaciones fiscales; estrella popular, orgullosa de serlo, pero víctima también de esa fama</p>{/* <p>{props.description}</p> */}
         {/* {props.description.length > x-characters && <button>ver más</button>} */}
 
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {/* {props.movies.filter(movie.cycle === props.id).forEach(movie => { */}
             {/* <MovieCard {...movie}/> */}
           {/* })} */}
