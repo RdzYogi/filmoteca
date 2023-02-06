c4ef9944bc98c55a3e9200eb513cad0f42fbfc2a
Silviu Rosu <silviurosu1988@gmail.com>
2023-02-06 12:46:24 +0100

    fixed img location to absolute root

--- app/javascript/containers/pages/Cycle.jsx
+++ app/javascript/containers/pages/Cycle.jsx
@@ -7,7 +7,7 @@ import MovieCard from '../../components/shared/MovieCard'
 function Cycle(props) {
   return (
     <Layout>
-      {/* <Navbar/> */}
+      <Navbar/>
       <div className="pt-40 max-w-7xl mx-auto pb-1 my-6">
         <h2 className="text-center font-bold text-2xl	pt-2 pb-2 bg-green-700">Douglas Sirk. LA EMOCIÓN INMEDIATA</h2>{/* <h2>{props.name}</h2> */}
         <p className="text-justify mt-3 mb-7">¿Se puede decir algo nuevo sobre esta gran estrella del mundo del espectáculo español? ¿Queda algo que podamos apuntar acerca de una persona que rompió todos los moldes en un tiempo en el que ser notoria aludía a un horizonte no apto para las mujeres? ¿Puede alguien ser merecedora de la Gran Cruz de la Orden de Isabel Católica, otorgada en pleno franquismo, y de la muy democrática Medalla de Oro al Mérito en el trabajo? ¿Cómo puede una persona reunir en sí tantas posibilidades? Pionera de la resistencia frente a la violencia de género y adalid de una minoría étnica; cabeza incontestable de familia, pero amante fogosa del amor y la pasión sexual; voz alzada frente al mercado de la droga, pero ciudadana procesada por no cumplir con sus obligaciones fiscales; estrella popular, orgullosa de serlo, pero víctima también de esa fama</p>{/* <p>{props.description}</p> */}
