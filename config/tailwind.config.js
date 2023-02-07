112174394f39e194a06d2be27ec20d4c0385a81e
Silviu <silviurosu1988@gmail.com>
2023-02-07 10:24:23 +0100

    switched font to Roboto

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        // sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Roboto', 'sans-serif'],
       },
     },
   },
