5f8cba6d61d7b351c9e7f4d3e53ac618bc9749df
Silviu Rosu <silviurosu1988@gmail.com>
2023-02-07 19:19:38 +0100

    cycle show with api info

--- app/javascript/containers/pages/Cycle.jsx
+++ app/javascript/containers/pages/Cycle.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
 import Footer from '../../components/navigation/Footer'
 import Navbar from '../../components/navigation/Navbar'
@@ -10,16 +10,16 @@ function Cycle() {
     let params = useParams()
     const slug = params.slug;
     // We declare a state so we can store the data from the API
-    const [cycle, setCycle] = useState([])
+    const [cycleData, setCycleData] = useState([])
     const [loaded, setLoaded] = useState(false)
     useEffect(() => {
       fetch(`http://localhost:3000/api/v1/cycles/${slug}`)
         .then((response) => response.json())
         .then((data) => {
-          console.log(data)
+          // console.log(data)
           // We set the state with the new array
           // and we set loaded to true so we can render the data
-          setCycle(data)
+          setCycleData(data)
           setLoaded(true)
         });
     }, [])
@@ -27,35 +27,39 @@ function Cycle() {
     <Layout>
       <Navbar/>
       <div className="pt-40 max-w-7xl mx-auto pb-1 my-6">
-        <h2 className="text-center font-bold text-2xl	pt-2 pb-2 bg-green-700">Douglas Sirk. LA EMOCIÓN INMEDIATA</h2>{/* <h2>{props.name}</h2> */}
-        <p className="text-justify mt-3 mb-7">¿Se puede decir algo nuevo sobre esta gran estrella del mundo del espectáculo español? ¿Queda algo que podamos apuntar acerca de una persona que rompió todos los moldes en un tiempo en el que ser notoria aludía a un horizonte no apto para las mujeres? ¿Puede alguien ser merecedora de la Gran Cruz de la Orden de Isabel Católica, otorgada en pleno franquismo, y de la muy democrática Medalla de Oro al Mérito en el trabajo? ¿Cómo puede una persona reunir en sí tantas posibilidades? Pionera de la resistencia frente a la violencia de género y adalid de una minoría étnica; cabeza incontestable de familia, pero amante fogosa del amor y la pasión sexual; voz alzada frente al mercado de la droga, pero ciudadana procesada por no cumplir con sus obligaciones fiscales; estrella popular, orgullosa de serlo, pero víctima también de esa fama</p>{/* <p>{props.description}</p> */}
-        {/* {props.description.length > x-characters && <button>ver más</button>} */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {/* {props.movies.filter(movie.cycle === props.id).forEach(movie => { */}
-            {/* <MovieCard movie={movie}/> */}
-          {/* })} */}
-          <MovieCard
-            title="Magnificent Obsession"
-            director="Douglas Sirk"
-            year="1954"
-            hall="Sala 1"
-            description="Un panadero que se ha hecho rico monta una fábrica de pasta. Un amigo, harto de su superficialidad, decide gastarle una broma y le cuenta que un príncipe va a ir a visitarla. Debut en el largometraje de Detlef Sierck/ Douglas Sirk, siguiendo el sistema de dobles versiones tan común en la época fue rodada también en holandés, aunque esa versión se ha perdido."
-          />
-          <MovieCard
-            title="Magnificent Obsession"
-            director="Douglas Sirk"
-            year="1954"
-            hall="Sala 1"
-            description="Un panadero que se ha hecho rico monta una fábrica de pasta. Un amigo, harto de su superficialidad, decide gastarle una broma y le cuenta que un príncipe va a ir a visitarla. Debut en el largometraje de Detlef Sierck/ Douglas Sirk, siguiendo el sistema de dobles versiones tan común en la época fue rodada también en holandés, aunque esa versión se ha perdido."
-          />
-          <MovieCard
-            title="Magnificent Obsession"
-            director="Douglas Sirk"
-            year="1954"
-            hall="Sala 1"
-            description="Un panadero que se ha hecho rico monta una fábrica de pasta. Un amigo, harto de su superficialidad, decide gastarle una broma y le cuenta que un príncipe va a ir a visitarla. Debut en el largometraje de Detlef Sierck/ Douglas Sirk, siguiendo el sistema de dobles versiones tan común en la época fue rodada también en holandés, aunque esa versión se ha perdido."
-          />
-        </div>
+        {/* First check if the data is loaded, then render */}
+        { loaded &&
+          <Fragment>
+            <h2 className={"text-center font-bold text-2xl	pt-2 pb-2 bg-" + cycleData.cycle.color}>{cycleData.cycle.name}</h2>
+            <p className="text-justify mt-3 mb-7">{cycleData.cycle.description}</p>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {/* {props.movies.filter(movie.cycle === props.id).forEach(movie => { */}
+                {/* <MovieCard movie={movie}/> */}
+              {/* })} */}
+              <MovieCard
+                title="Magnificent Obsession"
+                director="Douglas Sirk"
+                year="1954"
+                hall="Sala 1"
+                description="Un panadero que se ha hecho rico monta una fábrica de pasta. Un amigo, harto de su superficialidad, decide gastarle una broma y le cuenta que un príncipe va a ir a visitarla. Debut en el largometraje de Detlef Sierck/ Douglas Sirk, siguiendo el sistema de dobles versiones tan común en la época fue rodada también en holandés, aunque esa versión se ha perdido."
+              />
+              <MovieCard
+                title="Magnificent Obsession"
+                director="Douglas Sirk"
+                year="1954"
+                hall="Sala 1"
+                description="Un panadero que se ha hecho rico monta una fábrica de pasta. Un amigo, harto de su superficialidad, decide gastarle una broma y le cuenta que un príncipe va a ir a visitarla. Debut en el largometraje de Detlef Sierck/ Douglas Sirk, siguiendo el sistema de dobles versiones tan común en la época fue rodada también en holandés, aunque esa versión se ha perdido."
+              />
+              <MovieCard
+                title="Magnificent Obsession"
+                director="Douglas Sirk"
+                year="1954"
+                hall="Sala 1"
+                description="Un panadero que se ha hecho rico monta una fábrica de pasta. Un amigo, harto de su superficialidad, decide gastarle una broma y le cuenta que un príncipe va a ir a visitarla. Debut en el largometraje de Detlef Sierck/ Douglas Sirk, siguiendo el sistema de dobles versiones tan común en la época fue rodada también en holandés, aunque esa versión se ha perdido."
+              />
+            </div>
+          </Fragment>
+        }
       </div>
       <Footer/>
     </Layout>
