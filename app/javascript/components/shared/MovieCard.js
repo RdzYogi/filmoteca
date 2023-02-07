9c0a0bcab4c6bc6617a29d0fce3c4c80406ff0e0
Vito Lattanzio <vlattanzio13@gmail.com>
2023-02-07 19:49:27 +0100

    movie card update cycles

--- app/javascript/components/shared/MovieCard.js
+++ app/javascript/components/shared/MovieCard.js
@@ -2,12 +2,28 @@ import React, { useState } from "react"
 import { Navigate } from "react-router-dom"
 
 function MovieCard(movieData){
-  console.log(movieData.movie)
+
   const movie = movieData.movie
+  const movieTitle = movie.movie.title
+  const cycleColor = movie.include.cycle.color
+  // // const movieDirector = movie.director
+  // // const movieYear = movie.year
+  console.log(movie.include.cycle.color)
+
+  const showDateString = movie.include.session.play_time
+  const showDate = new Date(showDateString)
+  const optionMonth = { month: 'long' };
+  const showDateMonth = new Intl.DateTimeFormat('es-ES',optionMonth).format(showDate)
+  const showDateDay = showDate.getDay()
+  const showDateTime = showDate.getHours() + ':' + showDate.getMinutes()
+  const hall = movie.include.hall.name
+
   return (
     <div className="border border-black">
-      <p className="text-center font-bold text-xl">{movie.title}</p>
-      <p className="text-center">{movie.director} ({movie.year})</p>
+      <div className={"bg-"+cycleColor}>
+        <p className="text-center font-bold text-xl">{movieTitle}</p>
+      </div>
+      {/* <p className="text-center">{movieDirector} {movieYear}</p> */}
       {/* {movie.session.datetime.forEach(date =>{ */}
         {/*
         <div className="flex text-xs bg-black text-white pl-2 pr-2 pt-1 pb-1 justify-between items-center">
@@ -17,10 +33,13 @@ function MovieCard(movieData){
         */}
       {/* })} */}
       {/* <img src="movie.img_url" alt={movie.title}> */}
-      <div className="flex text-xs bg-black text-white pl-2 pr-2 pt-1 pb-1 justify-between items-center">
-        <p>17 enero</p> {/*delete later*/}
-        <p>18:00h</p> {/*delete later*/}
-        <p>{movie.hall}</p> {/*delete later*/}
+      <div className={"flex text-xs bg-black pl-2 pr-2 pt-1 pb-1 justify-between items-center text-white"}>
+        <div className={"flex justify-between text-"+cycleColor}>
+          <p>{showDateDay} {showDateMonth}</p>
+          <p>{showDateTime}h</p>
+          <p>{hall}</p>
+        </div>
+
         <p className="font-bold bg-green-700 p-1 ">Comprar</p> {/*delete later*/}
       </div>
       <img className="object-cover w-screen" src="https://media.istockphoto.com/id/835370890/photo/sunset-sunrise-with-clouds-light-rays-and-other-atmospheric-effect.jpg?s=612x612&w=0&k=20&c=zGDOBYVFY74wX2gUgkonYGtNl1zenev5mPotAqUlJbM="/>  {/*delete later*/}
