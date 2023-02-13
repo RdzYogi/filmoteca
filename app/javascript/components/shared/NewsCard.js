768676a4226c6bcddcd39b7c1e2da3ec5fe96b63
Francisco Crespo <fcrespo8@hotmail.com>
2023-02-13 14:10:19 +0100

    Starting working with MovieCard:starting searching for data; and Noticias.jsx :fetching

---
+++ app/javascript/components/shared/NewsCard.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function NewsCard(props) {
+
+  console.log(props.noticia.title)
+
+
+  return (
+    <div>NewsCard</div>
+  )
+}
+
+export default NewsCard
