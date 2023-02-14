8bc01ad165c21dc493901eb2750f88d32b0e4ca0
root <leonidastaousanis76@gmail.com>
2023-02-14 12:58:49 +0100

    a lot of errors still trying to make the calendar i got the current month displayed and trying to figure out how to use the database to display the informations

--- app/javascript/containers/pages/Calendar.jsx
+++ app/javascript/containers/pages/Calendar.jsx
@@ -1,16 +1,73 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Footer from '../../components/navigation/Footer'
 import Navbar from '../../components/navigation/Navbar'
 import Layout from '../../hocs/layouts/Layout'
 
 function Calendar() {
+  const date = new Date();
+  const month = date.toLocaleString('default', { month: 'long' });
+  const year = date.getFullYear();
+
+  const daysInMonth = new Date(year, date.getMonth() + 1, 0).getDate();
+
+  const days = [];
+  for (let i = 1; i <= daysInMonth; i++) {
+    days.push(i);
+  }
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('"api/v1/movies"')
+      .then(response => response.json())
+      .then(data => setMovies(data));
+  }, []);
+
   return (
     <Layout>
-      <Navbar/>
+      {/* <Navbar/> */}
       <div className="pt-40">Calendar</div>
+     <div>
+      <h2>{month} {year}</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Sun</th>
+            <th>Mon</th>
+            <th>Tue</th>
+            <th>Wed</th>
+            <th>Thu</th>
+            <th>Fri</th>
+            <th>Sat</th>
+          </tr>
+        </thead>
+        <tbody>
+          {days.map((day, index) => {
+            const dayOfWeek = new Date(year, date.getMonth(), day).getDay();
+            if (index === 0) {
+              return (
+                <tr>
+                  {[...Array(dayOfWeek).fill(null), day].map((cell, index) => (
+                    <td key={index} />
+                  ))}
+                </tr>
+              );
+            } else {
+              return (
+                index % 7 === 0 ?
+                  <tr key={index}>
+                    <td>{day}</td>
+                  </tr> :
+                  <td key={index}>{day}</td>
+              );
+            }
+          })}
+        </tbody>
+      </table>
+    </div>
       <Footer/>
     </Layout>
-  )
+  );
 }
 
 export default Calendar
