d5e0b6b197086d5100f7bfadddd08081eba78283
Laura Nguyen Trong <laurangt@yahoo.com>
2023-02-14 12:35:40 +0100

    movie fetch and display admin DB

--- app/javascript/containers/pages/AdminDB.jsx
+++ app/javascript/containers/pages/AdminDB.jsx
@@ -7,6 +7,7 @@ import MovieDB from '../../components/adminDB/MovieDB';
 
 function AdminDB() {
   const [ciclos, setCiclos] = useState([])
+  const [movies, setMovies] = useState([])
 
   useEffect(() => {
     fetch('/api/v1/cycles')
@@ -22,12 +23,28 @@ function AdminDB() {
       });
   }, [])
 
+  useEffect(() => {
+    fetch('/api/v1/movies')
+      .then((response) =>{
+        return response.json()
+      })
+      .then((data) => {
+        let newMoviesDB = []
+        data.map((movie, index) => {
+          newMoviesDB = [...newMoviesDB, <MovieDB key={index} movie={movie}/>]
+        })
+        setMovies(newMoviesDB)
+      });
+  }, [])
+
+
   return (
     <Layout>
       <Navbar/>
       <div className='pt-40 p-4 max-w-7xl mx-auto pb-1 my-6 md:px-12'>
         <h2 className="text-center text-2xl font-bold">Admin Database</h2>
         {ciclos}
+        {movies}
         {/* <CycleDB />
         <MovieDB /> */}
 
