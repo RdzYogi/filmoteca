444b8619d0c5c1b416e99b1040ef4d570340d861
Silviu <silviurosu1988@gmail.com>
2023-02-15 20:39:17 +0100

    added movie to carousel

--- app/javascript/containers/pages/Home.jsx
+++ app/javascript/containers/pages/Home.jsx
@@ -6,10 +6,11 @@ import Noticias from '../../components/home/Noticias'
 import MovieCarousel from '../../components/home/MovieCarousel'
 import Carousel from '../../components/home/Carousel'
 import CycleCard from '../../components/shared/CycleCard'
+import MovieCard from '../../components/shared/MovieCard'
 
 function Home() {
   const [ciclos, setCiclos] = useState([])
-  const [loaded, setLoaded] = useState(false)
+  const [movies, setMovies] = useState([])
   useEffect(() => {
     fetch('api/v1/cycles')
       .then((response) => response.json())
@@ -17,26 +18,37 @@ function Home() {
         data.map((cycle,index) => {
           setCiclos(ciclos => [...ciclos, <CycleCard key={index} cycle={cycle}/>])
         })
-        setLoaded(true)
+      });
+    fetch("api/v1/movies")
+    .then((response) => response.json())
+    .then((data) => {
+      let newMovies = []
+      data.map((movie,index) => {
+        newMovies = [...newMovies, <MovieCard key={index} movie={movie} cycle={movie.include.cycle}/>]
+      })
+      setMovies(newMovies)
       });
   }, [])
   return (
     <Layout>
       <Navbar/>
       <div className="pt-40 ">
-        <div className="flex justify-between" >
+        {/* <div className="flex justify-between" >
           <div className="">
             <h2 className="p-3 text-center font-bold text-2xl">Peliculas</h2>
-            {/* <MovieCarousel /> */}
+            <MovieCarousel />
           </div>
 
-          {/* <Noticias/> */}
-        </div>
+          <Noticias/>
+        </div> */}
+        <h2 className='text-center font-bold text-2xl pb-4'>Ciclos</h2>
         <div className="mx-auto mb-32 max-w-7xl">
           <Carousel slides={ciclos} />
-
         </div>
-
+        <h2 className='text-center font-bold text-2xl pb-4'>Peliculas</h2>
+        <div className="mx-auto mb-32 max-w-7xl">
+          <Carousel slides={movies} />
+        </div>
       </div>
       <Footer/>
     </Layout>
