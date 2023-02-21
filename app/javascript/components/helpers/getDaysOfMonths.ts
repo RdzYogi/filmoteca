4e6a265c610a66dafacd8c346f99a7d87af7ad35
Silviu Rosu <silviurosu1988@gmail.com>
2023-02-21 14:14:27 +0100

    started work on the component

--- app/javascript/components/helpers/getDaysOfMonths.ts
+++ app/javascript/components/helpers/getDaysOfMonths.ts
@@ -1,31 +1,41 @@
 
+type ResultObject = {
+  decemberYearBefore: Date[],
+  januaryYearAfter: Date[],
+  0: Date[], 1: Date[], 2: Date[], 3: Date[], 4: Date[],5: Date[], 6: Date[], 7: Date[], 8: Date[], 9: Date[], 10: Date[], 11: Date[]
+}
+
 function getDaysOfMonths() {
-  const result= {
-    0: [],
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-    5: [],
-    6: [],
-    7: [],
-    8: [],
-    9: [],
-    10: [],
-    11: [],
+  const result : ResultObject= {
+    decemberYearBefore:[],
+    januaryYearAfter:[],
+    0: [], 1: [], 2: [], 3: [], 4: [],5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [],
   }
   const year = new Date().getFullYear()
+
+  // Fill december of last year key
+  let dataLastYear = new Date(Date.UTC(year - 1, 11, 1));
+  while (dataLastYear.getUTCMonth() === 11) {
+    result["decemberYearBefore"].push(new Date(dataLastYear));
+    dataLastYear.setUTCDate(dataLastYear.getUTCDate() + 1);
+  }
+
+  // Fill january of next year
+  let dataNextYear = new Date(Date.UTC(year + 1, 0, 1));
+  while (dataNextYear.getUTCMonth() === 0) {
+    result["januaryYearAfter"].push(new Date(dataNextYear));
+    dataNextYear.setUTCDate(dataNextYear.getUTCDate() + 1);
+  }
+
   for (let month = 0; month < 12; month++) {
     let date = new Date(Date.UTC(year, month, 1));
     while (date.getMonth() === month) {
-      // Object.defineProperty(result, month, {
-      // });
       result[month].push(new Date(date));
       date.setUTCDate(date.getDate() + 1);
     }
   }
   console.log("result:",result)
-  return ""
+  return result
 }
 
 export default getDaysOfMonths
--- app/javascript/components/home/Calendar.jsx
+++ app/javascript/components/home/Calendar.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect } from 'react'
+import getDateObject from '../helpers/getDateObject'
 import getDaysOfMonth from '../helpers/getDaysOfMonths'
 
 function Calendar() {
   // console.log("Calendar", getDaysOfMonth())
   useEffect(() => {
-    getDaysOfMonth()
+    const dateInfo = getDaysOfMonth()
+
+    // Extract the month
+    const dateToday = new Date()
+    const dateTodayDateObject = getDateObject(dateToday,{monthLong:true})
+    const monthIndex = dateToday.getMonth()
   }, [])
   return (
     <div>Calendar</div>
