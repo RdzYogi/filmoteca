0c31addc026efab88e36a9ab2313f48492defc9e
ruben sanchez <sanchez.alvarez.ruben@gmail.com>
2023-02-22 13:42:43 +0100

    doneRuben

--- db/seeds.rb
+++ db/seeds.rb
@@ -86,6 +86,7 @@ Cycle.create(name: "FILMOTECA JUNIOR",
             slug: "ciclo-3",
             start_date: "2023-02-01",
             end_date: "2023-12-28")
+
 puts "Cycles created"
 
 # Define placeholder sessions
