a671649c5799591569f71630558303330769d94a
Francisco Crespo <fcrespo8@hotmail.com>
2023-02-24 11:40:59 +0100

    Cycle controller show fixed. Add column short to movies, migration ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_22_170331) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_103821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_170331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.text "shorts", default: ""
     t.index ["cycle_id"], name: "index_movies_on_cycle_id"
   end
 
