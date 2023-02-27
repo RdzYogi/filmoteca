f61cf3e6d49c4d118164122853fe735bc4873a61
Silviu <silviurosu1988@gmail.com>
2023-02-27 13:08:05 +0100

    initial state of weekday buttons

--- app/javascript/components/home/Calendar.jsx
+++ app/javascript/components/home/Calendar.jsx
@@ -7,9 +7,13 @@ let smallCalendarGrid = {}
 
 function Calendar({movies}) {
 
+  const calendarHelperObj = calendarHelper()
   const [currentMonth, setCurrentMonth] = useState('')
   const [calendarGrid, setCalendarGrid] = useState([])
   const [weekdays, setWeekdays] = useState([])
+
+  const [weekDetailsButtons, setWeekDetailsButtons] = useState([])
+
   const [currentWeek, setCurrentWeek] = useState("week-0")
 
   useEffect(() => {
@@ -29,12 +33,35 @@ function Calendar({movies}) {
       ])
     }
   }, [movies])
+
+  useEffect(() => {
+    if (calendarGrid.length === 0) return
+    const firstWeek = document.getElementById('week-0')
+    const children = firstWeek.children
+    for (let i = 0; i < children.length; i++) {
+      setWeekDetailsButtons(prev => [...prev,
+        <button key={i+"dayName"} className="text-white self-center justify-self-center bg-black h-fit w-fit px-3 " >
+          {calendarHelperObj.spanishWeekdays[i] + " " + children[i].innerText}
+        </button>
+      ])
+    }
+  }, [calendarGrid])
+
   const handleWeekChange = (e) => {
     e.preventDefault()
     const weeks = [document.getElementById('week-0'),document.getElementById('week-1'),document.getElementById('week-2'),document.getElementById('week-3'),document.getElementById('week-4')]
     weeks.map(week => {
       if (week.id === e.currentTarget.id) {
         week.classList.add('bg-gray-100')
+        const children = week.children
+        setWeekDetailsButtons([])
+        for (let i = 0; i < children.length; i++) {
+          setWeekDetailsButtons(prev => [...prev,
+            <button key={i+"dayName"} className="text-white self-center justify-self-center bg-black h-fit w-fit px-3 " >
+              {calendarHelperObj.spanishWeekdays[i] + " " + children[i].innerText}
+            </button>
+          ])
+        }
         setCurrentWeek(week.id)
       } else {
         week.classList.remove('bg-gray-100')
@@ -44,6 +71,12 @@ function Calendar({movies}) {
   return (
     <div className='flex justify-between max-w-7xl mx-auto pb-10 h-[32rem]'>
       <div className='w-3/4 bg-gray-200'>
+        <div className='w-full h-14 bg-gray-400 grid grid-cols-7'>
+          {weekDetailsButtons}
+        </div>
+        <div>
+
+        </div>
 
       </div>
       <div className='w-72 pt-10 mx-auto'>
