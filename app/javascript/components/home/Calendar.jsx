05738a9837aa923c284dae5195f14f9de9836d32
Silviu <silviurosu1988@gmail.com>
2023-02-28 20:01:57 +0100

    removed logs

--- app/javascript/components/home/Calendar.jsx
+++ app/javascript/components/home/Calendar.jsx
@@ -137,7 +137,6 @@ function Calendar({movies}) {
   }
 
   function handleDayChange (e) {
-    console.log("Movies: ", movies)
     e.preventDefault()
     let result = []
     const allButtons = e.currentTarget.parentElement.parentElement.children
@@ -178,7 +177,7 @@ function Calendar({movies}) {
     })
   }
   return (
-    <div className='flex flex-col-reverse lg:flex-row w-full max-w-7xl mx-auto pb-10 '>
+    <div className='flex flex-col-reverse lg:flex-row  max-w-7xl mx-auto pb-10 mx-2 '>
 
       <div className='w-full lg:w-3/4 bg-gray-100'>
         <div id="week-details-buttons-container" className='h-14 grid grid-cols-7'>
