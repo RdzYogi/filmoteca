0dab82ebd0abcd0b01ab8346690c50fa8167a59f
Laura Nguyen Trong <laurangt@yahoo.com>
2023-02-28 11:26:45 +0100

    correct seats but not dry yet

--- app/javascript/components/Halls/Seat.jsx
+++ app/javascript/components/Halls/Seat.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-function Seat(props) {
+function Seat() {
   const [isTaken, setIsTaken] = useState(false)
 
   const handleClick = () => {
@@ -9,7 +9,7 @@ function Seat(props) {
 
   return (
     <button onClick={handleClick} className={isTaken ? 'text-red-600 text-4xl mr-5' : 'text-4xl mr-5'}>
-      •{props.row}{props.column}
+      •
     </button>
   )
 }
