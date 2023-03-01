82a0db9c1b08b40fd978fc03dba2b9730d1787d9
Silviu <silviurosu1988@gmail.com>
2023-03-01 15:14:05 +0100

    removed logs

--- app/javascript/components/calendar/MobileCalendar.jsx
+++ app/javascript/components/calendar/MobileCalendar.jsx
@@ -88,16 +88,11 @@ function MobileCalendar({movies}) {
   }, [movies])
 
   const handleDayChange = (e) => {
-    // console.log(e.currentTarget.innerText)
     const daysContainer = document.getElementById('days-container')
     const dayClicked = Number(e.currentTarget.innerText)
-    console.log(dayClicked)
     const dayToDisplay = document.getElementById(dayClicked+1+"-day-to-display")
-    // dayToDisplay.scrollIntoView({behavior: "smooth", block: "nearest", inline: "nearest"})
     const offsetTop = dayToDisplay.offsetTop - daysContainer.offsetTop
-    // console.log(offsetTop *0.7)
     daysContainer.scrollTo({top:offsetTop,behavior:'smooth'})
-    console.log(dayToDisplay)
     const buttons = e.currentTarget.parentNode.childNodes
     for (let i = 0; i < buttons.length; i++) {
       buttons[i].classList.remove('border-blue-400')
