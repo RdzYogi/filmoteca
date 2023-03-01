96b3b1cb9f8d44b0878f63a4052e264239a46057
Silviu <silviurosu1988@gmail.com>
2023-03-01 14:15:47 +0100

    populated list

--- app/javascript/components/calendar/MobileCalendar.jsx
+++ app/javascript/components/calendar/MobileCalendar.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import createSmallCalendar from '../helpers/createSmallCalendar'
+import filterMoviesByDay from '../helpers/filterMoviesByDay'
+import getDateObject from '../helpers/getDateObject'
 
 function MobileCalendar({movies}) {
   const [currentMonth, setCurrentMonth] = useState('')
   const [calendarGrid, setCalendarGrid] = useState([])
   const [weekdays, setWeekdays] = useState([])
+  const [daysDetails, setDaysDetails] = useState([])
 
   useEffect(() => {
     if (movies.length === 0) return
@@ -24,6 +27,63 @@ function MobileCalendar({movies}) {
         setCalendarGrid(prev => [...prev, <button onClick={handleDayChange} className='h-10 w-10 rounded-full mb-2 flex justify-center border border-transparent' key={i+"day"}>{smallCalendarGrid.calendarGrid[i]}</button>])
       }
     }
+
+    // Create the days details scrollable section
+    const today = new Date()
+    const todayDateObj = getDateObject(today)
+
+    for (let i = 0; i < smallCalendarGrid.calendarGrid.length; i++) {
+      if (smallCalendarGrid.calendarGrid[i].key.split(' ')[1] === "currMonth") {
+        const day = Number(smallCalendarGrid.calendarGrid[i].key.split(' ')[0])+1
+        const filteredMovies = filterMoviesByDay({movies:movies.movies,day:day.toString()})
+        const newDate = new Date(todayDateObj.year,today.getMonth(),day)
+        const spanishDay = getDateObject(newDate.toDateString(),{dayLong:true}).day
+        const formattedSpanishDay = (spanishDay.charAt(0).toUpperCase()+spanishDay.slice(1)).slice(0,3)
+        if (filteredMovies.length === 0) {
+          setDaysDetails(prev => [...prev,
+            <div key={i+"day"} id={i+"-day-to-display"} className="flex justify-between pb-3">
+              <div className='w-[10%] flex flex-col items-center pl-2'>
+                <p>{formattedSpanishDay}</p>
+                <p>{Number(smallCalendarGrid.calendarGrid[i].key.split(' ')[0])+1}</p>
+              </div>
+              <div className='h-20 w-[85%] bg-blue-200 mt-2 ml-2' >No hay projections en ese dia</div>
+            </div>
+          ])
+        } else{
+          const result = []
+          filteredMovies.forEach((movie) => {
+            console.log(movie)
+            let playTimeString = ""
+            movie.props.movie.include.projections.forEach((projection) => {
+              const sessionPlayTime = getDateObject(projection.include.session.play_time)
+              console.log("algo",Number(sessionPlayTime.day))
+              if(day === Number(sessionPlayTime.day)){
+                playTimeString = sessionPlayTime.hour + ":" + sessionPlayTime.minutes + " H - " + projection.include.hall.name
+                console.log(playTimeString)
+              }
+            })
+            result.push(
+              <div key={movie.props.movie.movie.slug} className={'flex flex-col h-20 mb-2 bg-'+ movie.props.cycle.color } >
+                <p className='ml-2 font-bold'>{movie.props.movie.movie.title}</p>
+                <p className='ml-2'>{movie.props.movie.movie.director}</p>
+                <p className='ml-2'>{playTimeString}</p>
+              </div>
+            )
+          })
+          setDaysDetails(prev => [...prev,
+            <div key={i+"day"} id={i+"-day-to-display"} className="flex justify-between pb-3">
+              <div className='w-[10%] flex flex-col items-center pl-2'>
+                <p>{formattedSpanishDay}</p>
+                <p>{Number(smallCalendarGrid.calendarGrid[i].key.split(' ')[0])+1}</p>
+              </div >
+              <div className='flex flex-col mt-2 w-[85%]'>
+                {result}
+              </div>
+            </div>
+          ])
+        }
+      }
+    }
   }, [movies])
 
   const handleDayChange = (e) => {
@@ -36,7 +96,7 @@ function MobileCalendar({movies}) {
     e.currentTarget.classList.add('border-blue-400')
   }
   return (
-    <div className='w-72 pt-10 mx-auto'>
+    <div className='w-72 mx-auto'>
         {/* Calendar month */}
         <div className='text-black font-bold' >
           <p>{currentMonth.charAt(0).toUpperCase()+currentMonth.slice(1)}</p>
@@ -46,6 +106,10 @@ function MobileCalendar({movies}) {
         </div >
         <div className='grid grid-cols-7 place-items-center'>
           {calendarGrid}
+        </div>
+        <div className='w-full h-px bg-black mb-3'></div>
+        <div id="days-details" className='w-full h-60 bg-gray-200 overflow-x-auto pb-5 scroll-smooth'>
+          {daysDetails}
         </div>
     </div>
   )
