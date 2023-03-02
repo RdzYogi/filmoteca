378cb28d7c87dcfbabe8e0c447fea32b0a446499
Silviu Rosu <silviurosu1988@gmail.com>
2023-03-02 13:19:16 +0100

    fixed cycle controller

--- app/controllers/api/v1/cycles_controller.rb
+++ app/controllers/api/v1/cycles_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::CyclesController < ApplicationController
     # Cycle.include preloads the data from the database
     # So that when we render the json, it doesn't have to make
     # a new query to the database for each movie and session
-    cycles = Cycle.includes(:movies, :sessions).references(:movies, :sessions)
+    cycles = Cycle.includes(:movies).references(:movies)
 
     # We use the include option to tell the json renderer
     # to include the movies and sessions in the json
-    render json: cycles, include: %i[movies sessions]
+    render json: cycles, include: %i[movies]
   end
 
   def show
