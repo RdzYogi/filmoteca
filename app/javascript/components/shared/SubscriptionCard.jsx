ca13c326112c234e5fb01b706f4d8679e6e8ae05
ruben sanchez <sanchez.alvarez.ruben@gmail.com>
2023-03-03 15:20:53 +0100

    white box and span text added

--- app/javascript/components/shared/SubscriptionCard.jsx
+++ app/javascript/components/shared/SubscriptionCard.jsx
@@ -6,10 +6,10 @@ function SubscriptionCard(props) {
   const subscriptionType = subscription.tipo
   const remaining_uses = subscription.remaining_uses
   return (
-    <div className="border-solid border-1 border-white">
+    <div className="border-2 border-white-600">
       <ul>
-        <li className='px-2 py-3'>Tipo de abono: {subscriptionType}</li>
-        <li className='px-2 py-3'>Usos restantes: {remaining_uses}</li>
+        <li className='px-2 py-3'>Tipo de abono: <span className="font-bold">{subscriptionType}</span></li>
+        <li className='px-2 py-3'>Usos restantes: <span className="font-bold">{remaining_uses}</span></li>
       </ul>
     </div>
   );
