ba6ce3b689a4c7188ca3131bb5ddd45857776150
ruben sanchez <sanchez.alvarez.ruben@gmail.com>
2023-03-03 13:20:34 +0100

    background negro

--- app/javascript/containers/pages/UserDetails.jsx
+++ app/javascript/containers/pages/UserDetails.jsx
@@ -42,7 +42,7 @@ function UserDetails() {
               <h1 className="text-xl font-bold text-center">SUSCRIPCIONES</h1>
       </div>
       {/* <div className="pt-20">{currentUserStore.email}</div> */}
-      <div className="pt-10">
+      <div className="pt-10 bg-black">
         {loaded ? subscriptions : <div className="text-center">Cargando...</div>}
       </div>
       {/* <div className="pt-40">{authToken}</div> */}
