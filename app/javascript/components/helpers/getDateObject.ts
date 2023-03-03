7d8daab5da75ace0d7dd7b730aa2b43687f60b52
Silviu <silviurosu1988@gmail.com>
2023-03-03 15:13:40 +0100

    fixed getDateObject

--- app/javascript/components/helpers/getDateObject.ts
+++ app/javascript/components/helpers/getDateObject.ts
@@ -29,19 +29,19 @@ function getDateObject(dateString: string,
   } as DateObject
 
   if (dayLong) {
-    const intlDate = new Intl.DateTimeFormat('es-ES',{year: "numeric",month: optionMonth, weekday: "long",hour:"2-digit",minute: "2-digit"}).format(date)
+    const intlDate = new Intl.DateTimeFormat('es-ES',{year: "numeric",month: optionMonth, weekday: "long",day:"numeric",hour:"2-digit",minute: "2-digit"}).format(date)
     dateObject.month = monthLong ? intlDate.split(" ")[2] : intlDate.split("/")[1]
     dateObject.year = monthLong ? intlDate.split(",")[1].split(" ")[4] : intlDate.split(",")[1].split("/")[1]
     dateObject.day = intlDate.split(',')[0]
     dateObject.hour = intlDate.split(',')[1].split(' ')[1].split(':')[0]
-    dateObject.minutes = intlDate.split(',')[1].split(' ')[1].split(':')[1]
+    dateObject.minutes = intlDate.split(',')[2].split(' ')[1].split(':')[1]
     if (dateObject.minutes === "0"){
       dateObject.minutes = "00"
     }
   } else{
     const intlDate = new Intl.DateTimeFormat('es-ES',{year: "numeric",month: optionMonth, day: "2-digit",hour:"2-digit",minute: "2-digit"}).format(date)
     dateObject.month = monthLong ? intlDate.split(" ")[2] : intlDate.split("/")[1]
-    dateObject.year = monthLong ? intlDate.split(" ")[4] : intlDate.split("/")[1]
+    dateObject.year = monthLong ? intlDate.split(" ")[4] : intlDate.split("/")[2].split(",")[0]
     dateObject.day = monthLong ? intlDate.split(" ")[0] : intlDate.split("/")[0]
     dateObject.hour = intlDate.split(',')[1].split(' ')[1].split(':')[0]
     dateObject.minutes = intlDate.split(',')[1].split(' ')[1].split(':')[1]
