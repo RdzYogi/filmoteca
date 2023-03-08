d5c8542ee5bfffe3a51e8c4f9876d4ecc762ad74
ruben sanchez <sanchez.alvarez.ruben@gmail.com>
2023-03-08 11:25:15 +0100

    show logic done

--- app/javascript/containers/pages/Movie.jsx
+++ app/javascript/containers/pages/Movie.jsx
@@ -3,6 +3,33 @@ import React, { useEffect, useState } from 'react'
 import Footer from "../../components/navigation/Footer";
 import Navbar from "../../components/navigation/Navbar";
 import Layout from "../../hocs/layouts/Layout";
+import DownloadButton from "../../components/shared/DownloadButton";
+import getDateObject from "../../components/helpers/getDateObject";
+import { useSelector } from "react-redux";
+import MovieCard from "../../components/shared/MovieCard";
+import Carousel from "react-multi-carousel";
+import LeftArrow from "../../components/carousel/LeftArrow";
+import RightArrow from "../../components/carousel/RightArrow";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 3
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 768, min: 0 },
+    items: 1
+  }
+}
 
 function Movie() {
   // Get the slug from the URL
@@ -10,117 +37,81 @@ function Movie() {
   const slug = params.slug;
   // console.log(slug)
   // We declare a state so we can store the data from the API
-  const [cycleData, setCycleData] = useState([])
-  const [loaded, setLoaded] = useState(true)
+  const [projectionsData, setProjectionsData] = useState([])
+  const [loaded, setLoaded] = useState(false)
   const [movies, setMovies] = useState([])
+  const [mainMovie, setMainMovie] = useState({})
+  const moviesData= useSelector(state => state.dataManager.movies)
   useEffect(() => {
+
     fetch(`/api/v1/movies/${slug}`)
       .then((response) => response.json())
       .then((data) => {
-        // console.log(slug)
-        data.movie.map((movie,index) => {
-          // console.log(data)
-          setMovies(movies => [...movies, <MovieCard key={index} movie={movie} cycle={data.cycle}/>])
-        })
-        setCycleData(data)
+        //console.log(data.include.projections)
+        data.include.projections.forEach(projection => {
+          // console.log(projection)
+          //console.log(getDateObject(projection.include.session.play_time, {dayLong: true, monthLong: true}))
+          const dateObject = getDateObject(projection.include.session.play_time, {dayLong: true, monthLong: true})
+          const dateObjectDay = getDateObject(projection.include.session.play_time, {monthLong: true})
+          const capitalizedDay = dateObject.day.charAt(0).toUpperCase() + dateObject.day.slice(1)
+          setProjectionsData(projectionsData => [...projectionsData,
+            <div key={projection.projection.id}>
+              <h3>{projection.include.hall.name}</h3>
+              <h3>{capitalizedDay + ", " + dateObjectDay.day + " de " + dateObject.month + " " + dateObject.year}</h3>
+              <h3>{dateObject.hour + ":" + dateObject.minutes}</h3>
+            </div>
+          ])
+        });
+        setMainMovie(data)
         setLoaded(true)
       });
   }, [])
+  useEffect(() => {
+    if (moviesData.length === 0 || Object.keys(mainMovie).length === 0 ) return
+    //console.log(moviesData)
+    const currentCycle = mainMovie.include.cycle.id
+    moviesData.forEach((movie, index) => {
+      if (movie.include.cycle.id === currentCycle){
+        console.log(movie)
+        setMovies(prev => [...prev, <MovieCard key={index} movie={movie} cycle={movie.include.cycle}/> ])
+
+      }
+    })
+  }, [moviesData, mainMovie])
+
   return (
     <Layout>
       <Navbar />
       <div className="pt-40">
-        <div className="grid grid-cols-2 gap-4">
-          <div className="bg-"></div>
-          <div className="">
-            <h3 className="h3 font-bold">{cycleData.cycle.name}</h3>
-            <h3 className="h3 font-bold">Peli LA April</h3>
-            <h3 className="h3 font-medium">{movie.movie.director} Douglas Sirk <time>18 Nov 2022</time></h3>
+        {loaded &&
+        <div className="w-full mx-auto md:max-w-7xl ">
+          <div className="flex justify-between items-center">
+            <div className="flex">
+              <div className={"w-5 bg-"+mainMovie.include.cycle.color}></div>
+              <div className="ml-3">
+                <h3 className="h3 font-bold">{mainMovie.include.cycle.name}</h3>
+                <h3 className="h3 font-bold">{mainMovie.movie.title}</h3>
+                <h3 className="h3 font-medium">{mainMovie.movie.director+" ("+ mainMovie.movie.year+")"} </h3>
+              </div>
+            </div>
+          <DownloadButton />
           </div>
-          <div>
-            <img src="https://i1.wp.com/www.anim-arte.com/wp-content/uploads/2013/02/el-padrino.jpg?fit=1920%2C1080"
-            className="aspect-video w-full object-cover" alt="Imagen El Padrino"/>
-            <p>América, años 40. Don Vito Corleone (Marlon Brando) es el respetado
-            y temido jefe de una de las cinco familias de la mafia de Nueva
-            York. Tiene cuatro hijos: Connie (Talia Shire), el impulsivo Sonny
-            (James Caan), el pusilánime Fredo (John Cazale) y Michael (Al
-            Pacino), que no quiere saber nada de los negocios de su padre.
-            Cuando Corleone, en contra de los consejos de 'Il consigliere' Tom
-            Hagen (Robert Duvall), se niega a participar en el negocio de las
-            drogas, el jefe de otra banda ordena su asesinato. Empieza entonces
-            una violenta y cruenta guerra entre las familias mafiosas.s</p>
+          <div className="flex">
+            <div className="w-3/4">
+              <img src={mainMovie.movie.img_url}
+              className="aspect-video w-full object-cover" alt={mainMovie.movie.title}/>
+              <p>{mainMovie.movie.description}</p>
+            </div>
+            <div className="w-1/4 bg-gray-500">
+              <h3>PASES</h3>
+              {projectionsData}
+            </div>
           </div>
-          <div>
-            <p>PASES</p>
-            <p>SALA 1</p>
-            <p>FECHA</p>
-            <p>HORA</p>
-            <button className="inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-black-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-250 ease-in-out">
-            Comprar
-          </button>
-          </div>
-
+          <Carousel customLeftArrow={<LeftArrow/>} customRightArrow={<RightArrow/>} itemClass='flex justify-center' responsive = {responsive} className="mx-auto mb-32 max-w-7xl">
+            {movies}
+          </Carousel>
         </div>
-      </div>
-
-      <div className="">
-        <div className="">
-        <button className="inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-black-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-250 ease-in-out">
-            Atras
-        </button>
-        </div>
-        <div className="container m-5">
-          <div className="text-xl font-large text-black-900 m-2">
-            <div className="h-80 w-80 bg-white">
-              <h2 className="m-5 text-sm text-black underline font-bold">PASES</h2>
-                <ul className="list-disc m-5 text-sm text-black">
-                  <li>SALA 1</li>
-                    <li>FECHA</li>
-                    <li>HORA</li>
-                  </ul>
-          <button className="inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-black-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-250 ease-in-out">
-            Comprar
-          </button>
-          <ul className="list-disc m-5 text-sm text-black">
-                  <li>SALA 1</li>
-                    <li>FECHA</li>
-                    <li>HORA</li>
-                  </ul>
-          <button className="inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-black-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-250 ease-in-out">
-            Comprar
-          </button>
-          </div>
-        </div>
-      </div>
-      <div className="m-10 max-w-2xl">
-        <img src="https://i1.wp.com/www.anim-arte.com/wp-content/uploads/2013/02/el-padrino.jpg?fit=1920%2C1080"
-          className="aspect-video w-full object-cover" alt="Imagen El Padrino"/>
-        <div className="container px-50 color-green"></div>
-        <div className="m-5 col-start-4 max-w-2xl object-cover bg-green-cycle">
-          "nombre del ciclo"
-        </div>
-          <p className="m-5 max-w-2xl text-black-500 text-justify">
-            América, años 40. Don Vito Corleone (Marlon Brando) es el respetado
-            y temido jefe de una de las cinco familias de la mafia de Nueva
-            York. Tiene cuatro hijos: Connie (Talia Shire), el impulsivo Sonny
-            (James Caan), el pusilánime Fredo (John Cazale) y Michael (Al
-            Pacino), que no quiere saber nada de los negocios de su padre.
-            Cuando Corleone, en contra de los consejos de 'Il consigliere' Tom
-            Hagen (Robert Duvall), se niega a participar en el negocio de las
-            drogas, el jefe de otra banda ordena su asesinato. Empieza entonces
-            una violenta y cruenta guerra entre las familias mafiosas.
-          </p>
-        <div className="m-5">
-          <h2 className="pt-20 text-xl font-large font-bold text-black-900 m-2">
-            MAS PELICULAS DEL CICLO
-          </h2>
-          <div className="pt-25 grid grid-cols-3 gap-6 w-2xl h-60 ">
-            <div className="col-span-1  bg-gray-400">Pelicula 1</div>
-            <div className="col-span-1  bg-gray-400">Pelicula 2</div>
-            <div className="col-span-1  bg-gray-400">Pelicula 3</div>
-          </div>
-        </div>
-      </div>
+        }
       </div>
       <Footer />
     </Layout>
