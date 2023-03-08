8afb547815bc41030d550339b0d7ce6eddc11e45
Laura Nguyen Trong <laurangt@yahoo.com>
2023-03-08 14:37:42 +0100

    changed schema removed ticket and null:false from subscription in reservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_162946) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_133353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,11 +87,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_162946) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.boolean "ticket"
     t.bigint "session_id", null: false
     t.bigint "seat_id", null: false
     t.bigint "user_id", null: false
-    t.bigint "subscription_id", null: false
+    t.bigint "subscription_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["seat_id"], name: "index_reservations_on_seat_id"
