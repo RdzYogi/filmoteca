0483b7ca2aa92b03e631e9ad458ca567bf3e4ebb
Silviu <silviurosu1988@gmail.com>
2023-03-10 15:41:16 +0100

    hover effect on mobile calendar day

--- app/javascript/components/calendar/MobileCalendar.jsx
+++ app/javascript/components/calendar/MobileCalendar.jsx
@@ -22,12 +22,12 @@ function MobileCalendar({movies}) {
     const size = smallCalendarGrid.calendarGrid.length
     for (let i = 0; i < size; i++) {
       if (smallCalendarGrid.calendarGrid[i].props.children.length === 2 && smallCalendarGrid.calendarGrid[i].props.children[0].props.children === 1){
-        setCalendarGrid(prev => [...prev, <button onClick={handleDayChange} className={'h-10 w-10 rounded-full mb-2 flex justify-center border ' + borderSelectedDay} key={i+"day"}>{smallCalendarGrid.calendarGrid[i]}</button>])
+        setCalendarGrid(prev => [...prev, <button onClick={handleDayChange} className={'h-10 w-10 rounded-full mb-2 flex justify-center transition duration-300 hover:border-blue-600 border ' + borderSelectedDay} key={i+"day"}>{smallCalendarGrid.calendarGrid[i]}</button>])
       } else if(smallCalendarGrid.calendarGrid[i].props.children.length === undefined && smallCalendarGrid.calendarGrid[i].props.children !== ""){
         setCalendarGrid(prev => [...prev, <div className='h-10 w-10 rounded-full mb-2 flex justify-center border border-transparent text-gray-300' key={i+"day"}>{smallCalendarGrid.calendarGrid[i]}</div>])
       }
       else{
-        setCalendarGrid(prev => [...prev, <button onClick={handleDayChange} className='h-10 w-10 rounded-full mb-2 flex justify-center border border-transparent' key={i+"day"}>{smallCalendarGrid.calendarGrid[i]}</button>])
+        setCalendarGrid(prev => [...prev, <button onClick={handleDayChange} className='h-10 w-10 rounded-full mb-2 flex justify-center transition duration-300 hover:border-blue-600 border border-transparent' key={i+"day"}>{smallCalendarGrid.calendarGrid[i]}</button>])
       }
     }
 
