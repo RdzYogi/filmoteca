e3bd7913f7792dab0b7e52f90635f755ae7c75e0
Silviu <silviurosu1988@gmail.com>
2023-03-10 15:39:07 +0100

    hover effect on desktop calendar cards

--- app/javascript/components/calendar/CalendarCards.jsx
+++ app/javascript/components/calendar/CalendarCards.jsx
@@ -9,7 +9,7 @@ function CalendarCards({movie,projection}) {
     const playTime = projection.include.session.play_time
     const playTimeString = getDateObject(playTime).hour + ":" + getDateObject(playTime).minutes + "H" + " - " + projection.include.hall.name
     setMovieCard(movieCards => [...movieCards,
-      <Link to={"/movies/"+ movie.movie.slug} key={movie.movie.slug+projection.projection.id} className={"text-center text-sm border mb-1 border-black h-46 w-36 bg-" + movie.include.cycle.color}>
+      <Link to={"/movies/"+ movie.movie.slug} key={movie.movie.slug+projection.projection.id} className={"text-center text-sm border mb-1 transition duration-300 hover:opacity-75 border-black h-46 w-36 bg-" + movie.include.cycle.color}>
         <div className=''>
           <p className='font-bold'>{playTimeString}</p>
           <p className='font-bold text-xs'>{movie.movie.title}</p>
