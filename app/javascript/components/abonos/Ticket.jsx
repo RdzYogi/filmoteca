caa517f6002ed23bae7eb61a22fb86429d2f08f0
Silviu <silviurosu1988@gmail.com>
2023-03-16 11:40:30 +0100

    removed buy from abonos/entrada

--- app/javascript/components/abonos/Ticket.jsx
+++ app/javascript/components/abonos/Ticket.jsx
@@ -5,7 +5,9 @@ function Ticket(props) {
     <div className='flex justify-between p-3 my-3 bg-black text-white items-center'>
       <p className='w-1/2'>{props.name}</p>
       <p className='w-1/4 text-center'>{props.price}</p>
-      <button className='bg-white text-black p-1 w-1/4'>Comprar</button>
+      { props.name !== "ENTRADA SENCILLA" &&
+        <button className='bg-white text-black p-1 w-1/4'>Comprar</button>
+      }
     </div>
   )
 }
